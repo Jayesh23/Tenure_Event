4ce5031476672a5bd0cd9b53eb19fe2ce1ef6df2
Jayesh Kumar Meena <iamjayesh23@gmail.com>
2019-12-13 11:41:17 +0530

    New File

--- screenshot/server.py
+++ screenshot/server.py
@@ -1,5 +1,6 @@
-from flask import Flask 
+from flask import Flask, send_from_directory, send_file
 import pyscreenshot as ImageGrab
+from datetime import datetime
 
 app = Flask(__name__) 
   
@@ -10,9 +11,17 @@ app = Flask(__name__)
 # ‘/’ URL is bound with hello_world() function. 
 def hello_world(): 
     im = ImageGrab.grab()
-    im.save('screenshot.png')
-    # im.show()
-    return "success"
+    name = 'screenshot'+str(datetime.now())+'.png'
+    im.save(name)
+    return send_file(name, attachment_filename=name)
+
+
+    # response = send_from_directory(director='', filename='screenshot'+str(datetime.now())+'.png')
+    # response.headers['my-custom-header'] = 'my-custom-status-0'
+    # return response
+
+    # return im
+    # return "success"
 
 # main driver function 
 if __name__ == '__main__': 
